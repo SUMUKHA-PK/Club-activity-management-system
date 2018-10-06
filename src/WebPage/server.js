20288b31bab3873e9d3f442f39d86fc272f91177
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-10-06 11:36:55 +0530

    Quite done

--- src/WebPage/server.js
+++ src/WebPage/server.js
@@ -27,27 +27,7 @@ var server = http.createServer(function(request, response) {
                     response.end();  
                 }  
             });  
-            break;  
-        // case '/HtmlPage2.html':  
-        //     fs.readFile(__dirname + path, function(error, data) {  
-        //         if (error) {  
-        //             response.writeHead(404);  
-        //             response.write(error);  
-        //             response.end();  
-        //         } else {  
-        //             response.writeHead(200, {  
-        //                 'Content-Type': 'text/html'  
-        //             });  
-        //             response.write(data);  
-        //             response.end();  
-        //         }  
-        //     });  
-        //     break;  
-        // default:  
-        //     response.writeHead(404);  
-        //     response.write("opps this doesn't exist - 404");  
-        //     response.end();  
-        //     break;  
+            break;
     }  
 });  
-server.listen(8082);  
+server.listen(1991);  
