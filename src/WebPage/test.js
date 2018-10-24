8ed1fab7cfee7fcb0d16deefb3f8c8217c4e8c60
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-10-24 17:35:38 +0530

    hwriof

--- src/WebPage/test.js
+++ src/WebPage/test.js
@@ -1,4 +1,3 @@
-
 function formdata(){
 var search1= document.getElementById("search").value;
 document.writeln("searching for " + search1 + "<br>");
@@ -11,4 +10,3 @@ var query1= document.getElementById("query").value;
 document.writeln("query for " + query1 + "<br>");
 //console.log(query1);
 }
-
