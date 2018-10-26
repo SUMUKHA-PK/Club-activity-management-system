6681f216f0963eb18253973da17f351f679eea5a
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-10-26 20:45:24 +0530

    Progress!

--- src/WebPage/app.js
+++ src/WebPage/app.js
@@ -1,8 +1,9 @@
 var express=require('express');
 var app=express();
+var execute = require('../Database/exec_query.js');
 
 app.set('views',__dirname + './views');
-app.use(express.static(__dirname + '/JS'));
+app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
@@ -12,18 +13,11 @@ app.get('/',function(req,res){
     res.render('./index.html');
     });
     
-    // app.get('/search',function(req,res){
-    // // connection.query('SELECT first_name from TABLE_NAME where first_name like "%'+req.query.key+'%"',
-    // // function(err, rows, fields) {
-    // // if (err) throw err;
-    // // var data=[];
-    // // for(i=0;i<rows.length;i++)
-    // // {
-    // // data.push(rows[i].first_name);
-    // // }
-    // res.end(JSON.stringify(data));
-    // //});
-//});
+app.get('/search',function(req,res){
+
+    execute.exec_query("SELECT * FROM " + JSON.stringify(req.query.key));
+    console.log(JSON.stringify(req.query.key));
+});
 
 var server=app.listen(3000,function(){
     console.log("We have started our server on port 3000");
