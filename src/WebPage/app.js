087c9e42a085925162cef63a8e0079d7715d1abd
16CO207 <16co207.archana@nitk.edu.in>
2018-11-02 14:21:03 +0530

    updated

--- src/WebPage/app.js
+++ src/WebPage/app.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var app=express();
 var execute = require('../Database/query_runner.js');
-var D;
+
 app.set('views',__dirname + '/views');
 app.use(express.static(__dirname));
 app.set('view engine', 'ejs');
@@ -19,33 +19,43 @@ app.get('/search',function(req,res){
     console.log(JSON.stringify(req.query.key).replace(/\"/g, ""));
 });
 
-// app.get('/clicked_Student', async (req, res) => {
+app.get('/clicked_Student', async (req, res) => {
   
-//     try{
-//         var data = await execute.result("SELECT * FROM Student");
-//     }
-//     catch(e){
-//         throw e
-//     }
-//     res.render('./index.ejs', {data})
-// });
-
-console.log(D);
-
-app.post('/clicked_Customer', (req, res) => {
-    //const click = {clickTime: new Date()};
-    console.log("Clicked Customer!");
+    try{
+        var data = await execute.result("SELECT * FROM Student");
+    }
+    catch(e){
+        throw e
+    }
+    res.render('./index.ejs', {data})
 });
 
-app.post('/clicked_Events', (req, res) => {
-    //const click = {clickTime: new Date()};
-    console.log("Clicked Events!");
+app.get('/clicked_Student', async (req, res) => {
+  
+    try{
+        var data = await execute.result("SELECT * FROM Student");
+    }
+    catch(e){
+        throw e
+    }
+    res.render('./index.ejs', {data})
 });
 
-app.post('/clicked_Projects', (req, res) => {
-    //const click = {clickTime: new Date()};
-    console.log("Clicked Projects!");
+app.get('/clicked_Student', async (req, res) => {
+  
+    try{
+        var data = await execute.result("SELECT * FROM Student");
+    }
+    catch(e){
+        throw e
+    }
+    res.render('./index.ejs', {data})
 });
+
+
+
+
+
 
 var server=app.listen(3000,function(){
     console.log("We have started our server on port 3000");
